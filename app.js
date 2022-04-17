df05db25f6bc0ee376f7ab2ba4382a435aa47bb9
Round0000 <billybinche@gmail.com>
2022-04-17 16:23:02 +0200

    commit

--- app.js
+++ app.js
@@ -1,189 +1,81 @@
-const today = new Date();
-let currentMonth = today.getMonth();
-let currentYear = today.getFullYear();
-const selectYear = document.getElementById("year");
-const selectMonth = document.getElementById("month");
-
-let postToday = false;
-
-const months = [
-  "Janvier",
-  "Février",
-  "Mars",
-  "Avril",
-  "Mai",
-  "Juin",
-  "Juillet",
-  "Août",
-  "Septembre",
-  "Octobre",
-  "Novembre",
-  "Décembre",
-];
-
-const days = [
-  "Lundi",
-  "Mardi",
-  "Mercredi",
-  "Jeudi",
-  "Vendredi",
-  "Samedi",
-  "Dimanche",
-];
-
-function getCalendar(month, year) {
+function getDaysArray(start, end) {
   const arr = [];
-
-  const ui_month = document.createElement("h2");
-  ui_month.innerText = `${months[month]} ${year}`;
-  ui_month.classList.add("month-title");
-  ui_month.id = `${months[month]}-${year}`;
-
-  months_nav.innerHTML += `<li><a href="#${ui_month.id}">${months[month]} ${year}</li>`;
-
-  ui_calendar.append(ui_month);
-
-  let firstDay = new Date(year, month).getDay();
-
-  function daysInMonth(iMonth, iYear) {
-    return 32 - new Date(iYear, iMonth, 32).getDate();
+  for (
+    dt = new Date(start);
+    dt <= new Date(end);
+    dt.setDate(dt.getDate() + 1)
+  ) {
+    arr.push(new Date(dt));
   }
-
-  // creating all cells
-  let date = 1;
-  for (let i = 0; i < 6; i++) {
-    let currDay = firstDay === 0 ? 6 : firstDay - 1;
-
-    for (let j = 0; j < 7; j++) {
-      if (date > daysInMonth(month, year)) {
-        break;
-      } else {
-        if (
-          date === today.getDate() &&
-          year === today.getFullYear() &&
-          month === today.getMonth()
-        ) {
-          postToday = true;
-        }
-
-        if (postToday) {
-          ui_day = document.createElement("li");
-          ui_day.dataset.date = `${year}-${month + 1}-${date}`;
-          ui_day.dataset.dayofweek = days[currDay];
-
-          arr.push({
-            date: `${year}-${month + 1}-${date}`,
-            dayofweek: days[currDay],
-          });
-        }
-
-        if (currDay === 6) {
-          currDay = 0;
-        } else {
-          currDay++;
-        }
-
-        date++;
-      }
-    }
-  }
-
   return arr;
 }
 
-console.log(getCalendar(currentMonth, currentYear));
+function getCalendarData(start, end) {
+  const calendar = {};
 
-function next() {
-  currentYear = currentMonth === 11 ? currentYear + 1 : currentYear;
-  currentMonth = (currentMonth + 1) % 12;
-  getCalendar(currentMonth, currentYear);
-  console.log(getCalendar(currentMonth, currentYear));
+  getDaysArray(start, end).forEach((date) => {
+    calendar[date] = {
+      matin: null,
+      aprem: null,
+      soir: null,
+    };
+  });
+
+  return calendar;
 }
 
-function previous() {
-  currentYear = currentMonth === 0 ? currentYear - 1 : currentYear;
-  currentMonth = currentMonth === 0 ? 11 : currentMonth - 1;
-  getCalendar(currentMonth, currentYear);
-  console.log(getCalendar(currentMonth, currentYear));
+function getDayName(date = new Date(), locale = "fr-FR") {
+  return date.toLocaleDateString(locale, { weekday: "long" });
 }
-
-function jump() {
-  currentYear = parseInt(selectYear.value);
-  currentMonth = parseInt(selectMonth.value);
-  getCalendar(currentMonth, currentYear);
+function getMonthName(date = new Date(), locale = "fr-FR") {
+  return date.toLocaleDateString(locale, { month: "long" });
 }
-//
-
-// ui_day.innerHTML = `
-//           <span>${date}</span>
-//           <span>${days[currDay]}</span>
-
-//           <div class="options">
-//             <label><input name="${ui_day.dataset.date}" data-time="matin" type="checkbox">Matin</label>
-//             <label><input name="${ui_day.dataset.date}" data-time="aprem" type="checkbox">Aprèm</label>
-//             <label><input name="${ui_day.dataset.date}" data-time="soir" type="checkbox">Soir</label>
-//           </div>
-//         `;
-
-// ui_calendar.append(ui_day);
 
 //
 
+function displayCalendar(period) {
 
+  const list = document.createElement('ul');
 
-// showCalendar(currentMonth, currentYear);
-// calendArray = getCalendar(currentMonth, currentYear);
-
-// next();
-
-btn_prev.addEventListener("click", (e) => {
-  e.preventDefault();
-
-  previous();
-});
-btn_next.addEventListener("click", (e) => {
-  e.preventDefault();
-
-  next();
-});
-
-// Form validation
-function formData2obj(fd) {
-  const obj = {};
-  for (key of fd.keys()) {
-    obj[key] = fd.get(key);
+  function addMonthTitle(date) {
+    const newMonthTitle = document.createElement("h2");
+    newMonthTitle.innerText = getMonthName(date) + " " + date.getFullYear();
+    newMonthTitle.classList.add("month_title");
+    list.append(newMonthTitle);
   }
-  return obj;
+
+  const baseDates = Object.keys(period);
+
+  console.log(baseDates);
+
+  baseDates.forEach((date) => {
+    date = new Date(date);
+
+    if (
+      date.getDate() === 1 ||
+      date.getDate() === new Date(baseDates[0]).getDate()
+    ) {
+      addMonthTitle(date);
+    }
+
+    const newItem = document.createElement("li");
+    newItem.dataset.date = `${date.getFullYear()}-${
+      date.getMonth() + 1
+    }-${date.getDate()}`;
+    newItem.innerText = date.getDate() + " " + getDayName(date);
+
+    list.append(newItem)
+  });
+
+  document.body.append(list);
 }
 
-creation_form.addEventListener("submit", (e) => {
-  e.preventDefault();
+const base = getCalendarData("2022-4-26", "2022-5-9");
 
-  const data = formData2obj(new FormData(e.target));
+displayCalendar(base);
 
-  const selection = {};
-
-  const selectedDates = Object.keys(data);
-
-  selectedDates.forEach((date) => {
-    const dateEl = document.querySelector(`[data-date="${date}"]`);
-
-    const selectedTimes = dateEl.querySelectorAll("input:checked");
-    selection[dateEl.dataset.date] = [];
-
-    selectedTimes.forEach((input) => {
-      selection[dateEl.dataset.date].push(input.dataset.time);
-    });
-  });
-
-  console.log(selection);
-  console.table(selection);
-
-  Object.keys(selection).forEach((date) => {
-    const newOption = document.createElement("li");
-    newOption.innerText = date;
-    response_list.append(newOption);
-  });
+document.addEventListener("click", (e) => {
+  if (e.target.matches("li")) {
+    console.log(e.target.dataset.date);
+  }
 });
-
-//
